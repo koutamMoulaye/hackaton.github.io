ee68fbe39a95c8495e10932dba8d0e40944017c7
waranomed <108696638+waranomed@users.noreply.github.com>
2022-07-04 22:34:02 +0200

    Add files via upload

---
+++ exoweb/main.c
@@ -0,0 +1,10 @@
+// la commande #include permet d'installer des librairies qui sont dans notre exemple <stdio.h>, <stdlib.h>
+// <stdio.h>, <stdlib.h> sont des librairies qui nous permet d'afficher du text facilement
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("bonjour");
+    return 1;
+}
